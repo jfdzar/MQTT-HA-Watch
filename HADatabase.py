405a9fb39f68e6d9818045658eafe9184cdb6e55
jfdzar@gmail.com <jfdzar@gmail.com>
2020-12-12 19:55:31 +0100

    Shortcut to create e-mail with log statistics

--- HADatabase.py
+++ HADatabase.py
@@ -3,6 +3,7 @@ import sqlite3
 import pandas as pd
 import shutil
 import os
+from datetime import date
 
 
 def convert_to_float(x):
@@ -73,44 +74,114 @@ class HADatabase:
     def read_database(self):
         """ Read Database File in Path """
 
-        try:
-            logging.info(
-                'Reading Database -  Reading HomeAssistan Database File')
-            self.df_db = read_db(self.path)
-        except Exception as e:  # skipcq: PYL-W0703
-            logging.error("Error Reading Database - Reading database file")
-            logging.error(e)
+        # try:
+        #     logging.info(
+        #         'Reading Database -  Reading HomeAssistan Database File')
+        #     self.df_db = read_db(self.path)
+        # except Exception as e:  # skipcq: PYL-W0703
+        #     logging.error("Error Reading Database - Reading database file")
+        #     logging.error(e)
 
-        try:
-            if self.df_csv.empty:
-                self.df_csv = self.df_db
-            else:
-                self.df_csv = self.df_csv.combine_first(self.df_db)
-        except Exception as e:  # skipcq: PYL-W0703
-            logging.error(
-                "Error Reading Database - Error combining DataFrames")
-            logging.error(e)
+        # try:
+        #     if self.df_csv.empty:
+        #         self.df_csv = self.df_db
+        #     else:
+        #         self.df_csv = self.df_csv.combine_first(self.df_db)
+        # except Exception as e:  # skipcq: PYL-W0703
+        #     logging.error(
+        #         "Error Reading Database - Error combining DataFrames")
+        #     logging.error(e)
 
         self.create_statistics()
 
     def create_statistics(self):
 
-        df = self.df_csv
-        txt = 'HomeAssistant Values Summary\n'
+        # df = self.df_csv
+        # txt = 'HomeAssistant Values Summary\n'
 
-        for x in df['entity_id'].unique():
-            no_sensor_values = "False" in str(
-                df[df['entity_id'] == x]['float'].value_counts().to_dict())
-            if (x[:7] == "sensor.") and not no_sensor_values:
-                txt += 'Entity: %s \n' % x
-                df_lv = df[df['entity_id'] == x].tail(
-                    self.statistics_sample)
-                txt += 'First Value: %s \n' % str(
-                    df_lv['created'].to_list()[0])
-                txt += 'Last Value:%s \n ' % str(
-                    df_lv['created'].to_list()[-1])
-                txt += 'Min: %2.2f \n' % df_lv['state'].min()
-                txt += 'Mean: %2.2f \n' % df_lv['state'].mean()
-                txt += 'Max: %2.2f \n' % df_lv['state'].max()
+        # for x in df['entity_id'].unique():
+        #     no_sensor_values = "False" in str(
+        #         df[df['entity_id'] == x]['float'].value_counts().to_dict())
+        #     if (x[:7] == "sensor.") and not no_sensor_values:
+        #         txt += 'Entity: %s \n' % x
+        #         df_lv = df[df['entity_id'] == x].tail(
+        #             self.statistics_sample)
+        #         txt += 'First Value: %s \n' % str(
+        #             df_lv['created'].to_list()[0])
+        #         txt += 'Last Value:%s \n ' % str(
+        #             df_lv['created'].to_list()[-1])
+        #         txt += 'Min: %2.2f \n' % df_lv['state'].min()
+        #         txt += 'Mean: %2.2f \n' % df_lv['state'].mean()
+        #         txt += 'Max: %2.2f \n' % df_lv['state'].max()
+
+        # self.email_txt = txt
+
+        df = pd.read_csv('HA-Watch.log', sep=' ', skiprows=415268,
+                         error_bad_lines=False, names=['Day', 'Time', 'Feed', 'Value'])
+        df['Feed'] = df['Feed'].apply(lambda s: s[:-1])
+        df['Time'] = df['Time'].apply(lambda s: s[:-1])
+
+        df['Value'] = df['Value'].apply(lambda x: convert_to_float(x))
+
+        df['Timestamp'] = df['Day'] + ' '+df['Time']
+        df['Timestamp'] = pd.to_datetime(
+            df['Timestamp'], format="%d-%m-%y %H:%M:%S")
+
+        txt = 'HomeAssistant Values Summary\n\n'
+
+        txt = 'Sensor Values\n\n'
+
+        topics = ['bath/temperature', 'bath/humidity',
+                  'kitchen/temperature', 'kitchen/humidity',
+                  'living/temperature', 'living/humidity',
+                  'pihumboldt/temperature', 'pitv/temperature']
+
+        for topic in topics:
+            try:
+                today_date = date.today().strftime("%m/%d/%Y")
+
+                df_today = df[df["Timestamp"] >= (
+                    pd.to_datetime(today_date) - pd.Timedelta(days=1))]
+
+                min_v = df_today[df_today['Feed'].str.contains(
+                    topic) & df_today['Value'] > 0].min()[3]
+                min_t = df_today[df_today['Feed'].str.contains(
+                    topic) & df_today['Value'] > 0].min()[4]
+
+                max_v = df_today[df_today['Feed'].str.contains(
+                    topic) & df_today['Value'] > 0].max()[3]
+                max_t = df_today[df_today['Feed'].str.contains(
+                    topic) & df_today['Value'] > 0].max()[4]
+
+                mean_v = df_today[df_today['Feed'].str.contains(
+                    topic) & df_today['Value'] > 0]['Value'].mean()
+
+                if 'temperature' in topic:
+                    unit = '°C'
+                elif 'humidity' in topic:
+                    unit = '%'
+                else:
+                    unit = ''
+
+                txt += "%s\n" % (topic)
+                txt += "Mean Value: %2.1f%s\n" % (mean_v, unit)
+                txt += "Min: %2.1f%s (%s)\n" % (min_v, unit, str(min_t))
+                txt += "Max: %2.1f%s (%s)\n\n" % (max_v, unit, str(max_t))
+            except Exception as e:  # skipcq: PYL-W0703
+                logging.error("Error evaluating Stats")
+                logging.error(e)
+
+        topics = ['bath/leak', 'kitchen/leak',
+                  'living/pir', 'door/door-status']
+
+        for topic in topics:
+            try:
+                txt += "%s\n" % (topic)
+                txt += str(df_today[df_today['Feed'].str.contains(topic)
+                                    & df_today['Value'] > 0]['Value'].value_counts())
+                txt += '\n\n'
+            except Exception as e:  # skipcq: PYL-W0703
+                logging.error("Error evaluating Stats")
+                logging.error(e)
 
         self.email_txt = txt
